7009ed5ded0d318817769bf58f19bc668da89505
BRIKSI Reda <briksireda@gmail.com>
2020-12-29 14:30:37 +0100

    initial commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,54 @@
+﻿/*using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+*/
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            //return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            //return HtmlEncoder.Default.Encode($"Bonjour {name}, ID: {ID}");
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["ID"] = ID;
+
+            return View();
+        }
+    }
+}
